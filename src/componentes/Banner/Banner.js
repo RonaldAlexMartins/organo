660ab017078956a15f648fff03619ae9ccef4b31
RonaldAlexMartins <ronny_nascimento@hotmail.com>
2023-03-01 20:49:42 -0300

    Alterando forma de exportar componente

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -1,13 +1,10 @@
 import './Banner.css'
 
-function Banner() {
+export const Banner = () => {
     //JSX
     return (
       <header className="banner"> 
         <img src="/imagens/banner.png" alt="banner principal da pagina do Organo"/>
       </header>
-          
     )
 }
-
-export default Banner
