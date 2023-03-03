2a6e45b83ef3fe806c8f6a78b88749470e389b1d
RonaldAlexMartins <ronny_nascimento@hotmail.com>
2023-03-02 21:15:51 -0300

    Construção do Componente ListaSuspensa

---
+++ src/componentes/ListaSuspensa/index.js
@@ -0,0 +1,13 @@
+const ListaSuspensa = (props) => {
+  console.log(props.item)
+  return (
+    <div>
+      <label>{props.label}</label>
+      <select>
+        {props.itens.map(item => <option>{item}</option>)}
+      </select>
+    </div>
+  );
+};
+
+export default ListaSuspensa;
