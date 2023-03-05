b01da01733745360eba343632a462590951095b5
RonaldAlexMartins <ronny_nascimento@hotmail.com>
2023-03-05 17:46:40 -0300

    Estilizando o Time

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,11 +1,13 @@
-import './Time.css'
+import "./Time.css";
 
 const Time = (props) => {
+  const css = { backgroundColor: props.corSecundaria }
+  
   return (
-    <section className='time'>
-      <h3>{props.nome}</h3>
+    <section className="time" style={css}>
+      <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
     </section>
-  )
-}
+  );
+};
 
-export default Time
+export default Time;
