754638a3f09b5960800ac2ff36f2f864e224124d
RonaldAlexMartins <ronny_nascimento@hotmail.com>
2023-03-14 20:34:48 -0300

    construindo componente colaborador

---
+++ src/componentes/colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return (<div>
+      <div>
+        <img src='https://github.com/viniciosneves.png' alt='Vinicios Neves'/>
+      </div>
+      <div>
+        <h4>Vinicius Neves</h4>
+        <h5>Instrutor</h5>
+      </div>
+    </div>
+
+    )
+}
+
+export default Colaborador
