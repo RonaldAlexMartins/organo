0bf4b8ea8d19f9bf8565d916f288486c050def1d
Ronald Martins <ronny_nascimento@hotmail.com>
2023-03-27 05:35:35 -0300

    Correções do projeto

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -8,7 +8,7 @@ const Time = (time, colaboradores) => {
     colaboradores.length > 0 && <section className="time" style={css}>
       <h3 style={{ borderColor: time.corPrimaria }}>{time.nome}</h3>
       <div className="colaboradores">
-        {colaboradores.map( colaborador => <Colaborador corDeFundo = {time.corPrimaria} key={colaborador.nome} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem}/> )}
+        {colaboradores.map( (colaborador, indice) => <Colaborador corDeFundo = {time.corPrimaria} key={indice} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem}/> )}
       </div>            
     </section>
 
