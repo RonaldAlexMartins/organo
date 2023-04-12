e7b0c25a004deb964d432555c65d0bc10433b057
Ronald Martins <ronny_nascimento@hotmail.com>
2023-04-11 22:13:27 -0300

    finalização do projeto

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,14 +1,14 @@
 import Colaborador from "../colaborador";
 import "./Time.css";
 
-const Time = (props, time, colaboradores) => {
-  const css = { backgroundColor: time.corSecundaria, backgroundImage: 'url(/imagens/fundo.png)' }  
+const Time = (props) => {
+  const css = { backgroundColor: props.corSecundaria, backgroundImage: 'url(/imagens/fundo.png)' }  
   return (
     
     props.colaboradores.length > 0 && <section className="time" style={css}>
-      <h3 style={{ borderColor: time.corPrimaria }}>{time.nome}</h3>
+      <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
       <div className="colaboradores">
-        {props.colaboradores.map( (colaborador, indice) => <Colaborador corDeFundo = {time.corPrimaria} key={indice} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem}/> )}
+        {props.colaboradores.map( (colaborador, indice) => <Colaborador corDeFundo = {props.corPrimaria} key={indice} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem}/> )}
       </div>            
     </section>
 
